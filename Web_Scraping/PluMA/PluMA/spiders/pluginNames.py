3e26d4320fcb9f2019e6cbc7871fc36709ce1add
Edward Palermo <eddx540@gmail.com>
2020-10-05 21:56:59 -0400

    Scrape data cleaned pt.1
    - Fixed duplicates from printing into the CSV file.
    - Renamed few variables in index.html.
    
    - Next task:
         - Filter out all empty spaces, new lines, etc.

--- Web_Scraping/PluMA/PluMA/spiders/pluginNames.py
+++ Web_Scraping/PluMA/PluMA/spiders/pluginNames.py
@@ -15,38 +15,62 @@ class PluginnamesSpider(Spider):
     custom_settings = {'FEED_URI' : 'results.csv'}
 
     def parse(self, response):
-        categories = response.xpath('.//table/tr/a') 
-        trs = categories.xpath('//tr') [1:]
-
-        for tr in trs:
         
-            # Obtain all plugin names
-            name = tr.xpath('.//td/u/a//text()').extract()
-            description = tr.xpath('.//tr/td//text()').extract()
-            language = tr.xpath('.//td//text()').extract()
+        tables = response.xpath('//div[contains(@id,"bigcolumn")]//table')
+        trs = tables.xpath('//tr')[4:]
 
-            #Writes to text file.
-            #yield{
-                #'Name' : name,
-                #'Description' : description,
-                #'Language' : language
-            #}
+
+        cycle = 0
+     
+        for tr in trs:
+            # Obtain all plugin names
+            name = tr.xpath('.//td[1]//text()').extract_first()
+            description = tr.xpath('.//td[2]//text()').extract_first()
+            language = tr.xpath('.//td[3]//text()').extract_first()
+
+            if (name == "AbundCSV2NOA"):
+                cycle = cycle + 1
+
+            if (cycle == 2):
+                name.remove('')
+                description.remove('')
+                language.remove('')
+                                   
+                break
+
+            #Writes to text file.  
+            yield{
+                'Name' : name,
+                'Description' : description,
+                'Language' : language
+            }
+
+                
+        
 
     #def parse_details(self, response):
-##
-            l = ItemLoader(item=PlumaItem(), response=response)
-        
-            #l.add_value('Name', name)
-            #l.add_value('Description', description)
-            l.add_value('Language', language)
 
-            return l.load_item()
-
-        return l.load_item()
+##            l = ItemLoader(item=PlumaItem(), response=response)    
+##            l.add_value('Name', name)
+##            l.add_value('Description', description)
+##            l.add_value('Language', language)
 ##            
+##            return l.load_item()
+##        
+##        return l.load_item()
+            
         
 
 
+
+#All tables and rows.
+#response.xpath('//div[contains(@id,"bigcolumn")]//table//text()').extract()
+
+#fFirst row
+#tr.xpath('.//td//text()').extract()
+
+# Name of row.
+# tr.xpath('.//td[1]//text()').extract()
 
 
   
